b16e19c1992d2d7af3ce179a7a8a003bdec1a630
Leonardo Schmitt <66500082+LeonardoSchmittK@users.noreply.github.com>
2022-10-03 21:30:53 -0300

    feature test

---
+++ src/main/java/com/schmitt/demo/MeuPrimeiroControler.java
@@ -0,0 +1,18 @@
+package com.schmitt.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/teste")
+public class MeuPrimeiroControler {
+    @GetMapping("/nome")
+    public String getMeuNome(){
+        return "Leonardo :)";
+    }
+    @GetMapping("/idade")
+    public int getMinhaIdade(){
+        return 22;
+    }
+}
